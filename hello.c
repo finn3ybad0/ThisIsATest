840f4fb6b02dfd2a76dbb7398cea63acc879304b
Finney Bado <finney.bado@icloud.com>
2024-03-15 14:34:01 -0400

    added an hello world script

---
+++ hello.c
@@ -0,0 +1,14 @@
+//
+//  hello.c
+//  ThisIsATest
+//
+//  Created by Finney on 2024-03-15.
+//
+
+#include <stdio.h>
+
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
